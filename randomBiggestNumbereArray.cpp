7f9aa88dd12df069572a7c6f5fc4edae19decc67
Jackson Naufal <Jackson.naufal@mths.ca>
2022-06-06 17:58:28 +0000

    arrayRowColumnAverager

--- randomBiggestNumbereArray.cpp
+++ randomBiggestNumbereArray.cpp
@@ -1,46 +1,57 @@
 // Copyright (c) 2022 Jackson Naufal All rights reserved
 // Created By Jackson Naufal
 // Created On March 2022
+// array, column, and row adder
 
 #include <iostream>
 #include <random>
 #include <array>
 
-template<size_t N>
-int BiggestNumber(std::array<int, N> array) {
-    // this function will find the largest number out of the 10
-    int biggestNumber = 0;
+template <int row, int columns>
+int ArrayMean(int (&randomGrid)[row][columns]) {
+    // this function calculates the mean of all the elements in a 2D array
+
+    int sumOfArrayNumbers = 0;
+
     // process
-    for (int counter = 0; counter < array.size(); counter++) {
-        if (biggestNumber < array[counter]) {
-            biggestNumber = array[counter];
-        }
+    for (size_t rowVal = 0; rowVal < row; rowVal++) {
+        for (size_t columnVal = 0; columnVal < columns; columnVal++)
+            sumOfArrayNumbers += randomGrid[rowVal][columnVal];
     }
-    return biggestNumber;
- }
+    int arraySize = sizeof(randomGrid)/sizeof(randomGrid[0]);
+    sumOfArrayNumbers = sumOfArrayNumbers / arraySize;
+
+    return sumOfArrayNumbers;
+}
 
 
-main() {
-    // this function will gather the 10 random numbers
-    std::array<int, 10> randomNum;
-    int randomNumber;
-    int biggestNum = 0;
+int main() {
+    // this function creates a 2D array
+
+    int randomNumber = 0;
+    int roundedArrayMean;
+    const int row = 4;
+    const int columns = 4;
+    int matrix[row][columns];
 
     std::random_device rseed;
-    std::mt19937 rgen(rseed());  // mersenne_twister
-    std::uniform_int_distribution<int> idist(0, 100);  // [0,9]
-
+    std::mt19937 rgen(rseed());
+    // mersenne_twister
+    std::uniform_int_distribution<int> idist(0, 50);
+    // [0, 50]
 
     // output
-    for (int counter = 0; counter <= 11; counter++) {
-        randomNumber = idist(rgen);
-        randomNum[counter] = randomNumber;
-        std::cout << "The random number is " << randomNumber << std::endl;
+    for (int rowVal = 0; rowVal < row; rowVal++) {
+        for (int columnVal = 0; columnVal < columns; columnVal++) {
+            randomNumber = idist(rgen);
+            matrix[rowVal][columnVal] = randomNumber;
+            std::cout << randomNumber << ", ";
+        }
+        std::cout << std::endl;
     }
 
-        biggestNum = BiggestNumber(randomNum);
-
-        std::cout << "\nThe biggest number is " << biggestNum << std::endl;
-
-    std::cout << "\nDone." << std::endl;
+    roundedArrayMean = ArrayMean(matrix);
+    std::cout << "" << std::endl;
+    std::cout << "The mean of the matrix is: "
+    << roundedArrayMean << std::endl;
 }
